a066edee51fe5f6dcca72cbad72643e7ec3bfb1a
Pierre Marty <p.marty@me.com>
2017-10-04 14:32:05 +0200

    Swift 4

--- WP/String+Convenience.swift
+++ WP/String+Convenience.swift
@@ -12,27 +12,30 @@ import UIKit
 // original idea from https://stackoverflow.com/questions/25607247/how-do-i-decode-html-entities-in-swift
 
 extension String {
-    init(htmlEncodedString: String) {
-        self.init()
-        if let attributedString = htmlEncodedString.htmlToAttributedString() {
-            self = attributedString.string
-        }
-        else {
-            self = htmlEncodedString
-        }
-    }
-
-    func htmlToAttributedString() -> NSAttributedString? {
-        if let data = self.data(using:.utf8) {
-            let options:[String : Any] = [NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType,
-                                          NSCharacterEncodingDocumentAttribute: String.Encoding.utf8.rawValue]
-            if let attributedString = try? NSAttributedString(data:data, options:options, documentAttributes:nil) {
-                return attributedString
-            }
-        }
-        return nil
-    }
-    
+	init(htmlEncodedString: String) {
+		self.init()
+		if let attributedString = htmlEncodedString.htmlToAttributedString() {
+			self = attributedString.string
+		}
+		else {
+			self = htmlEncodedString
+		}
+	}
+	
+	func htmlToAttributedString() -> NSAttributedString? {
+		if let data = self.data(using:.utf8) {
+			// pm171004 converted to Swift 4
+			let options:[NSAttributedString.DocumentReadingOptionKey : Any] =
+				[NSAttributedString.DocumentReadingOptionKey(rawValue: NSAttributedString.DocumentAttributeKey.documentType.rawValue): NSAttributedString.DocumentType.html,
+				 NSAttributedString.DocumentReadingOptionKey(rawValue: NSAttributedString.DocumentAttributeKey.characterEncoding.rawValue): String.Encoding.utf8.rawValue]
+			
+			if let attributedString = try? NSAttributedString(data:data, options:options, documentAttributes:nil) {
+				return attributedString
+			}
+		}
+		return nil
+	}
+	
 }
 
 
